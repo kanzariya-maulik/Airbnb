519318019179870adcd3aa904a3776aa856a5526
kanzariya-maulik <mkanzariya566@rku.ac.in>
2024-11-27 19:42:16 +0530

    update and delete back-end

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const ejs=require("ejs");
 const path = require("path");
+const methodOverride=require("method-override");
 const listing = require("./models/listings.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.urlencoded({extended : true}));
-app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 async function main() {
     mongoose.connect("mongodb://127.0.0.1:27017/airbnb");
@@ -40,10 +41,29 @@ app.get("/listing/new",(req,res)=>{
     res.render("newListing.ejs");
 });
 
+app.put("/listing/:id",async (req,res)=>{
+    let {id} = req.params;
+    let result = await listing.findByIdAndUpdate(id,{...req.body});
+    res.redirect(`/listing/${id}`);
+});
+
 app.get("/listing/:id",async (req,res)=>{
     let {id} = req.params;
     let listingData =await listing.findById(id);
     res.render("listing.ejs",{list:listingData});
+});
+
+app.get("/listing/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    let listingData =await listing.findById(id);
+    res.render("Edit.ejs",{list:listingData});
+});
+
+app.delete("/listing/:id",async (req,res)=>{
+    let {id} = req.params;
+    let deletedData= await listing.findByIdAndDelete(id);
+    console.log(deletedData);
+    res.redirect("/listing");
 });
 
 app.post("/listing",async (req,res)=>{
